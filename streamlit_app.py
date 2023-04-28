7503d14acf409500f7e7f201cd3485d6fab70e64
micben7 <micben7@gmail.com>
2023-04-28 12:36:11 +0100

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with st.expander("About this App"):
 #################################################################################################
 # Sidebar section ---
 with st.sidebar:
-    st.header("Streamlit Elements Corner ✨")
+    st.header("Livestock✨")
     select = st.selectbox("Explore Categories 👀", ["", "Data and Chart displays","Text Elements", "Input data", "Media 📷", "Layouts & Containers"])
     
     if st.button('Streamlit Challenge 🏁'):
