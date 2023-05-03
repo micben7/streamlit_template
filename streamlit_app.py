cfd08927e6090f38196f704a020e16e7896f30df
micben7 <micben7@gmail.com>
2023-05-04 00:41:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def main():
     #################################################################################################
     # Sidebar section ---
     with st.sidebar:
-        logo = Image.open("PICs/Alltech_logo.png")
+        logo = Image.open("pics/Alltech_logo.png")
         st.sidebar.image(logo, width=200)
         st.header("Price Insight App✨")
         select = st.selectbox("Explore Categories 👀", ["Home", "Livestock","Horticulture", "Grain","About Us", "Ratings 📷"])
