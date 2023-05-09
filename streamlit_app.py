19322f48dd95526a2b581df25f1b71792862ff17
micben7 <micben7@gmail.com>
2023-05-09 15:38:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,9 +83,8 @@ def main():
        # select_1 = st.selectbox("Select Product 👀", ["Home", "Horticulture","About Us", "Ratings 📷"])
          # User-based preferences
         st.write('Insight from Horticulture Dataset')
-        movie_1 = st.selectbox('Select Market',title_list[14930:15200])
-        movie_2 = st.selectbox('Select Product',title_list[25055:25255])
-        movie_3 = st.selectbox('Select period',title_list[21100:21200])
+        #movie_1 = st.selectbox('Select Market',title_list[14930:15200])
+        #movie_3 = st.selectbox('Select period',title_list[21100:21200])
         #fav_movies = [movie_1,movie_2,movie_3]
 
     ################################################################################################
