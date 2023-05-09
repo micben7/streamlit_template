8830fcb90986249714604fdbe8e29b3d40f4a168
micben7 <micben7@gmail.com>
2023-05-09 11:26:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def main():
         # generates random numeric values!
         df = pd.DataFrame(
             np.random.randn(50, 20),
-            columns=('col %d' % i for i in range(5))
+            columns=('col %d' % i for i in range(20))
         ) 
         st.dataframe(df) 
         
