b89660324c92606f8aaa3c6225de3bb1f31b5dad
micben7 <micben7@gmail.com>
2023-05-09 09:31:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from utils.data_loader import load_movie_titles
 
 
 # Data Loading
-title_list = load_movie_titles('Data/CSV files/Bloemfontein_(Mangaung)_Fresh_Produce_Market_(BLO)_combined')
+title_list = load_movie_titles('Data/CSV_Files/Bloemfontein_(Mangaung)_Fresh_Produce_Market_(BLO)_combined')
 def main():
     
     st.title('Alltech Inc.🎈') 
@@ -84,7 +84,7 @@ def main():
          # User-based preferences
         st.write('Insight from Horticulture Dataset')
         movie_1 = st.selectbox('Select Market',title_list)
-        #movie_2 = st.selectbox('Select Product',title_list[25055:25255])
+        movie_2 = st.selectbox('Select Product',title_list[25055:25255])
         #movie_3 = st.selectbox('Select period',title_list[21100:21200])
         #fav_movies = [movie_1,movie_2,movie_3]
 
