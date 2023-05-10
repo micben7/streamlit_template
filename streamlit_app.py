a721dd261d0393ebce99b467845f680e92308610
micben7 <micben7@gmail.com>
2023-05-10 09:57:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,8 @@ def main():
     # Text display section --->
     elif select == 'Horticulture':
         st.header('Insight from Horticulture Dataset')
-        choice = st.selectbox("Select Market 👇", ["Bloemfontein_(Mangaung)_Fresh_Produce_Market_(BLO)_combined", "new_pigs", "George_Fresh_Produce_Market_(GEO)_combined", "Johannesburg_Fresh_Produce_Market_(JOH)_combined", "Kimberley_(Sol_Plaatje)_Fresh_Produce_Market_(KIM)_combined", "Klerksdorp_Fresh_Produce_Market_(KLE)_combined", "Mpumalanga_Fresh_Produce_Market_(NEA)_combined", "Mthatha_(Kei)_Fresh_Produce_Market_(UMT)_combined", "Nelspruit_Fresh_Produce_Market_(NEL)_combined", "Pietermaritzburg_Fresh_Produce_Market_(PIE)_combined","Caption"])
+        choice = st.selectbox("Select Market 👇", ["Bloemfontein_(Mangaung)_Fresh_Produce_Market_(BLO)_combined","Cape Town Fresh Produce Market(CAP)", "East London Fresh Produce Market (EAS)", "Durban Fresh Produce Market(DUR)", "George_Fresh_Produce_Market_(GEO)_combined", "Johannesburg_Fresh_Produce_Market_(JOH)_combined", "Kimberley_(Sol_Plaatje)_Fresh_Produce_Market_(KIM)_combined", "Klerksdorp_Fresh_Produce_Market_(KLE)_combined", "Mpumalanga_Fresh_Produce_Market_(NEA)_combined", "Mthatha_(Kei)_Fresh_Produce_Market_(UMT)_combined", "Nelspruit_Fresh_Produce_Market_(NEL)_combined", "Pietermaritzburg_Fresh_Produce_Market_(PIE)_combined","Port Elizabeth Fresh Produce Market (POR)","Springs Fresh Produce Market(SPR)","Tshwane Fresh Produce Market(PRE)", "Vereeniging Fresh Produce Market (VER)", "Welkom (Matjhabeng) Fresh Produce Market (WEL)", "Witbank Fresh Produce Market (WIT)" 
+])
         
         if choice == 'Bloemfontein_(Mangaung)_Fresh_Produce_Market_(BLO)_combined':
             st.write('')
